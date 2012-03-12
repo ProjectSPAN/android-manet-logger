68faf9d508438e0ab4c9ccf2518de8acf39b2dba
nmodly <nmodly@mitre.org>
2012-03-11 20:47:42 -0400

    Initial commit of empty android logger project.

---
+++ src/android/adhoc/manet/logger/AndroidManetLoggerActivity.java
@@ -0,0 +1,13 @@
+package android.adhoc.manet.logger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidManetLoggerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
