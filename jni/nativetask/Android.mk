3a062ff4858a0d75947ce775eb0cd7b27e04da1c
jrobble <jrobble@mitre.org>
2012-06-20 01:36:11 -0400

    Deleted redundant files.

--- jni/nativetask/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libnativetask
-
-LOCAL_SRC_FILES := android_adhoc_manet_nativ_NativeTask.c
-
-include $(BUILD_SHARED_LIBRARY)
